46a0dc2cc803928a7c5e2a0e36d6c623951ee6ce
santiagofp <sfernandezpacheco@itba.edu.ar>
2024-10-11 23:39:40 -0300

    Update ej7.c

--- TP8/ej7.c
+++ TP8/ej7.c
@@ -83,12 +83,12 @@ posicion * resolverSopa(char *diccionario[], char sopa[][COLS]) {
     posicion * solaux;
     int i = 0;
     int w=0;
-    for(; diccionario[i][0] != '\0'; i++) {
+    for(; diccionario[i][0] != '\0'; i++) { //recorro el diccionario buscando palabra a palabra
         int fila = 0, columna = 0;
-        int direccionquebusco=0;
+        int direccionquebusco=0; //la paso como parametro para luego no volver a buscar una palabra donde ya la busque y darle el valor a la estructura
         do {
-            solaux = pasopalabra(diccionario[i], sopa, &fila , &columna, &direccionquebusco);
-            if(solaux != NULL) {
+            solaux = pasopalabra(diccionario[i], sopa, &fila , &columna, &direccionquebusco); //guardo la estructura en solaux
+            if(solaux != NULL) { //si es distinto de NULL significa que encontre la palabra
                 if (w % 10 == 0) {
                     solucion = realloc(solucion, sizeof(posicion) * (w + BLOQUE)); //esto tampoco lo entiendo
                 }
@@ -96,56 +96,56 @@ posicion * resolverSopa(char *diccionario[], char sopa[][COLS]) {
                 w++;
             free(solaux);
             }
-        } while (solaux != NULL);
+        } while (solaux != NULL); //esto es para que si se encontró la palabra que la siga buscando por si aparece otra vez
     }
-    solucion[w].palabra = NULL;
+    solucion[w].palabra = NULL; 
     return solucion;
 }
 posicion * pasopalabra(char *diccionario, char sopa[][COLS], int * fila, int * columna, int * direccionquebusco) {
-    posicion * resultado = malloc(sizeof(posicion));
+    posicion * resultado = malloc(sizeof(posicion)); //esto tampoco lo entiendo
     int direcciones[8][2] = { {1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
-    for (int i = *direccionquebusco; i < 8; i++) {
-        *direccionquebusco = i;
+    for (int i = *direccionquebusco; i < 8; i++) { //recorro las 8 direcciones y ademas i = direccionquebusco por si una palabra aparece mas de 1 vez para no volver a buscar donde ya se que no esta
+        *direccionquebusco = i; //guardo la direccion que estoy buscando para luego guardarlo en la estructura
         resultado = buscopalabra(diccionario,sopa,direcciones[i], fila , columna);
-        if (resultado != NULL) {
-            resultado->direccion = *direccionquebusco;
-            return resultado;
+        if (resultado != NULL) { // si resultado es != de NULL significa que encontro la palabra
+            resultado->direccion = *direccionquebusco; //guardo en la estructura la direccion y ya me quedan las 4 variables cubiertas
+            return resultado; //devuelvo la estructura
         }
     }
     *direccionquebusco = 0;
-    return resultado;
+    return resultado; //esto seria como devolver NULL porque si encuentro la palabra la devuelvo antes
 }
 posicion * buscopalabra(char *diccionario, char sopa[][COLS], int *direccion, int * fila, int * columna) {
     int x,y;
-    posicion * aparece = NULL;
+    posicion * aparece = NULL; //esto no lo entiendo
     int w=0;
     for(int i = *fila; i<FILS; i++) {
-        for (int j = *columna ; j < COLS; j++) {
+        for (int j = *columna ; j < COLS; j++) { //recorro la matriz buscando si la primera letra de la palabra coincide con la que hay en la posicion de la matriz
             if (sopa[i][j] == diccionario[w]) {
-                y=i;
-                x=j;
-                w=0;
-                while (diccionario[w] != 0 && y >= 0 && y < FILS && x >= 0 && x < COLS && sopa[y][x] == diccionario[w]) {
-                    if(diccionario[w+1] == 0) {
-                        aparece = malloc(sizeof(posicion));
+                y=i; //guardo el valor de las filas en y para luego si encuentro la palabra no perder el lugar donde estaba la primera letra
+                x=j; // guardo el valor de las columnas en x para luego si encuentro la palabra no perder el lugar donde estaba la primera letra
+                w=0; //asigno 0 a la w para ir recorriendo la palabra
+                while (diccionario[w] != 0 && y >= 0 && y < FILS && x >= 0 && x < COLS && sopa[y][x] == diccionario[w]) { //me fijo que no me haya salido de la matriz y por ultimo que coincidan las letras
+                    if(diccionario[w+1] == 0) { //si la siguiente letra es el 0 es que ya encontro toda la palabra
+                        aparece = malloc(sizeof(posicion)); //esto lo entiendo maso menos
                         aparece->columna=j;
                         aparece->fila=i;
-                        aparece->palabra = malloc(strlen(diccionario)+1);
-                        strcpy(aparece->palabra,diccionario);
-                        *fila = i;
-                        *columna = j+1;
-                        return aparece;
+                        aparece->palabra = malloc(strlen(diccionario)+1); //le doy a palabra espacio suficiente para copiar la palabra
+                        strcpy(aparece->palabra,diccionario); //copio la palabra en la estructura que voy a devolver
+                        *fila = i; //le doy a *fila el valor de i para que cuando vuelva a recorrer la matriz no recorra nuevamente lugares que ya recorrió
+                        *columna = j+1; //le doy a *columna el valor de j+1 para que cuando vuelva a buscar arranque por la siguiente letra a la anterior
+                        return aparece; //devuelvo la estrctura con el valor de la fila y columan donde aparece la palabra y que palabra es o sea solo me queda pendiente en que direccion aparece
                     }
-                        x+=direccion[0];
+                        x+=direccion[0]; 
                         y+=direccion[1];
                         w++;
                 }
-                w=0;
+                w=0; 
             }
         }
     }
     *fila = 0;
-    *columna = 0;
-    return NULL;
+    *columna = 0; //les vuelvo a poner el valor de 0 para que cuando vuelva a buscar por otra direccion arranque desde el principio de la matriz
+    return NULL; //si encontre la palabra la devolvi antes
 }
 
