eeb408e6dfb5d34dce592dce6cfdbbb8ca6dc96f
santiagofp <sfernandezpacheco@itba.edu.ar>
2024-10-12 01:10:02 -0300

    Create 14.c

---
+++ TP9/14.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <assert.h>
+
+int apareados(const char *texto);
+
+int main(void) {
+
+    assert(apareados("")==0);
+    assert(apareados("()")==0);
+    assert(apareados("(())()(()())")==0);
+    assert(apareados("((((()))))")==0);
+
+    assert(apareados("(")!=0);
+    assert(apareados(")")!=0);
+    assert(apareados("(")!=0);
+    assert(apareados("())")!=0);
+    assert(apareados("()(")!=0);
+    assert(apareados("((()))())(")!=0);
+
+    int dim=100000;
+    char s[dim+1];
+    for(int i=0; i<dim/2; i++) {
+        s[i] = '(';
+        s[dim-1-i] = ')';
+    }
+    s[dim]=0;
+    assert(apareados(s)==0);
+    for(int i=1; i < dim/100; i++) {
+        assert(apareados(s+i)!=0);
+    }
+
+    puts("OK!");
+    return 0;
+}
+int apareados(const char *texto){
+    if(*texto == 0)
+        return 0;
+
+    int aux = apareados(texto + 1);
+
+    if(aux < 0)
+        return aux;
+
+    return aux + (*texto == '(' ? -1 : 1);
+    // Este ultimo paso sirve SOLO porque el texto son unicamente parentesis
+}
